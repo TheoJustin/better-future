3ee9bf805738b509d483bc069b6966c31495be35
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2025-11-07 22:35:07 +0700

    menu deposit

--- BetterFuture/components/auth/AuthButton.tsx
+++ BetterFuture/components/auth/AuthButton.tsx
@@ -18,15 +18,28 @@ export function AuthButton({
   return (
     <button
       className={cn(
-        'bg-[#1899d6] block cursor-pointer h-[66px] overflow-hidden relative rounded-[13px] shrink-0 w-full',
-        'hover:bg-[#1cb0f6] transition-colors',
+        variant === 'primary'
+          ? 'bg-[#1899d6] hover:bg-[#1cb0f6]'
+          : 'bg-gray-200 hover:bg-gray-300',
+        'block cursor-pointer h-[66px] overflow-hidden relative rounded-[13px] shrink-0 w-full',
+        'transition-colors',
         'disabled:opacity-50 disabled:cursor-not-allowed',
         className,
       )}
       {...props}
     >
-      <div className="absolute bg-[#1cb0f6] box-border flex gap-2 h-[57px] items-center justify-center left-[0.5px] overflow-hidden px-4 py-3 right-[0.5px] rounded-[13px] top-1/2 -translate-y-1/2 w-[calc(100%-1px)]">
-        <div className="flex flex-col justify-center leading-[100%] relative shrink-0 text-base text-white text-center tracking-[-0.32px] uppercase whitespace-nowrap font-bold">
+      <div
+        className={cn(
+          'absolute box-border flex gap-2 h-[57px] items-center justify-center left-[0.5px] overflow-hidden px-4 py-3 right-[0.5px] rounded-[13px] top-1/2 -translate-y-1/2 w-[calc(100%-1px)]',
+          variant === 'primary' ? 'bg-[#1cb0f6]' : 'bg-gray-100',
+        )}
+      >
+        <div
+          className={cn(
+            'flex flex-col justify-center leading-[100%] relative shrink-0 text-base text-center tracking-[-0.32px] uppercase whitespace-nowrap font-bold',
+            variant === 'primary' ? 'text-white' : 'text-gray-700',
+          )}
+        >
           {children}
         </div>
       </div>
--- BetterFuture/components/home/HomeLayout.tsx
+++ BetterFuture/components/home/HomeLayout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { cn } from '@/lib/utils'
@@ -8,6 +8,7 @@ import { HomeHeader } from './HomeHeader'
 import { GreetingSection } from './GreetingSection'
 import { CoinDisplay } from './CoinDisplay'
 import { HomeBottomNav } from './HomeBottomNav'
+import { DepositModal } from './DepositModal'
 
 interface HomeLayoutProps {
   children?: ReactNode
@@ -17,40 +18,59 @@ interface HomeLayoutProps {
 
 export function HomeLayout({ children, coinAmount, className }: HomeLayoutProps) {
   const router = useRouter()
+  const [showDepositModal, setShowDepositModal] = useState(false)
 
   function handleMenuClick() {
     router.push('/?tab=menu')
   }
 
+  function handleDepositClick() {
+    setShowDepositModal(true)
+  }
+
+  function handleDepositSuccess() {
+    // Refresh balance or do something after successful deposit
+    // The CoinDisplay component will automatically refresh
+  }
+
   return (
-    <div
-      className={cn(
-        'relative w-[430px] h-[932px] mx-auto',
-        'bg-white overflow-hidden rounded-[55px] shadow-2xl',
-        className,
-      )}
-    >
-      {/* Control Bar */}
-      <div className="absolute bg-black h-[5px] left-[137px] rounded-[16px] top-[919px] w-[156px]" />
+    <>
+      <div
+        className={cn(
+          'relative w-[430px] h-[932px] mx-auto',
+          'bg-white overflow-hidden rounded-[55px] shadow-2xl',
+          className,
+        )}
+      >
+        {/* Control Bar */}
+        <div className="absolute bg-black h-[5px] left-[137px] rounded-[16px] top-[919px] w-[156px]" />
 
-      {/* Floating Island */}
-      <div className="absolute bg-black h-[37px] left-[152px] rounded-[23px] top-[11px] w-[126px]" />
+        {/* Floating Island */}
+        <div className="absolute bg-black h-[37px] left-[152px] rounded-[23px] top-[11px] w-[126px]" />
 
-      {/* Header */}
-      <HomeHeader onMenuClick={handleMenuClick} />
+        {/* Header */}
+        <HomeHeader onMenuClick={handleMenuClick} />
 
-      {/* Greeting Section */}
-      <GreetingSection />
+        {/* Greeting Section */}
+        <GreetingSection />
 
-      {/* Coin Display */}
-      <CoinDisplay coinAmount={coinAmount} />
+        {/* Coin Display */}
+        <CoinDisplay coinAmount={coinAmount} />
 
-      {/* Bottom Navigation */}
-      <HomeBottomNav />
+        {/* Bottom Navigation */}
+        <HomeBottomNav onDepositClick={handleDepositClick} />
 
-      {/* Additional children content */}
-      {children}
-    </div>
+        {/* Additional children content */}
+        {children}
+      </div>
+
+      {/* Deposit Modal */}
+      <DepositModal
+        isOpen={showDepositModal}
+        onClose={() => setShowDepositModal(false)}
+        onSuccess={handleDepositSuccess}
+      />
+    </>
   )
 }
 
